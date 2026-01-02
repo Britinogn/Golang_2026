14a3717ad1a63001e7174ede7d8f95cf0dcce741
Tino Nova Tech <124672998+Britinogn@users.noreply.github.com>
2026-01-02 07:18:15 +0100

    Refactor investment calculator and update README for clarity

--- go-project/ReadMe.md
+++ go-project/ReadMe.md
@@ -1,1 +1,3 @@
-built a system that can store and many future investment
+This project is focused on learning the fundamentals of Go. Using variables , types , functions etc, to build a system  that can return values of the product. 
+
+So in this case , the system is built on calculating and storing of investors investment for future references
--- go-project/investment_calculator.go
+++ go-project/investment_calculator.go
@@ -1,30 +1,49 @@
 package main
 
-import(
+import (
 	"fmt"
 	"math"
 )
 
+const inflationRate = 2.5
+
+
 func main() {
-	const inflationRate = 2.5
-	var investmentAmount float64 
+	var investmentAmount float64
 	expectedReturnRate := 5.5
 	var years float64
 
-	fmt.Print("Investment Amount: ")
+	outputText("Investment Amount: ")
 	fmt.Scan(&investmentAmount)
 
-	fmt.Print("Expected Return Rate: ")
+	outputText("Expected Return Rate: ")
 	fmt.Scan(&expectedReturnRate)
 
-	fmt.Print("Years: ")
+	outputText("Years: ")
 	fmt.Scan(&years)
 
-	futureValue := (investmentAmount) * math.Pow(1 + expectedReturnRate / 100, years)
-	futureRealValue := futureValue / math.Pow(1 + inflationRate/100, years)
+	futureValue,futureRealValue := calculateFutureValue(investmentAmount,expectedReturnRate,years)
 	
-	
-	fmt.Println(futureValue, "\n", futureRealValue)
+	formattedFV := fmt.Sprintf("Future value: %.1f\n", futureValue)
+	formattedRFV := fmt.Sprintf("Future value(inflation): %.1f\n", futureRealValue)
+
+	// outputs information
+	// fmt.Printf("Future value: %v\nFuture value(inflation): %v\n", futureValue, futureRealValue)
+	// fmt.Printf("Future value: %.1f\nFuture value(inflation): %.1f", futureValue, futureRealValue)
+
+	fmt.Print(formattedFV, formattedRFV)
+
+}
+
+func outputText(text string) {
+	fmt.Print(text)
+}
+
+func calculateFutureValue(investmentAmount, expectedReturnRate, years float64)(float64, float64){
+	fv := investmentAmount * math.Pow(1+expectedReturnRate/100,years)
+	rfv := fv / math.Pow(1+inflationRate/100, years)
+
+	return fv ,rfv
 }
 
 //variables types and operator
