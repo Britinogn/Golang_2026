f22167fb6ffb1fa2334fe21e2314d6c3ae958570
Tino Nova Tech <124672998+Britinogn@users.noreply.github.com>
2026-01-02 16:32:02 +0100

    Implement file-based account balance management and enhance user interaction for banking operations

--- go-bank/bank.go
+++ go-bank/bank.go
@@ -1,9 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const accountBalanceFile = "balance.txt"
+
+func getBalanceFromFile() float64 {
+	data , _ := os.ReadFile(accountBalanceFile)
+	balanceText := string(data)
+	balance , _ := strconv.ParseFloat(balanceText, 64)
+	return balance
+}
+
+func writeBalanceToFile(balance float64){
+	balanceText := fmt.Sprint(balance)
+	os.WriteFile(accountBalanceFile, []byte(balanceText), 0644)
+}
+
+//0644 ; this helps with file permission
 
 func main() {
-	var accountBalance = 1000.0
+	//we shouldn't store the account balance in numbers , we will be using file
+	var accountBalance = getBalanceFromFile()
+	
 	fmt.Println("Welcome to Go Bank")
 
 	// for key word for looping 
@@ -21,47 +43,48 @@ func main() {
 
 		//wantsCheckBalance := choice == 1
 
-		//if statement
-		if choice == 1{
-			fmt.Println("Your balance is:", accountBalance)
+		//switch
+		switch choice{
+			case 1:
+				fmt.Println("Your balance is:", accountBalance)
+			case 2:
+				fmt.Print("Your deposit: ")
+				var depositAmount float64
+				fmt.Scan(&depositAmount)
 
-		} else if choice == 2 {
-			fmt.Print("Your deposit: ")
-			var depositAmount float64
-			fmt.Scan(&depositAmount)
+				if depositAmount <= 0{
+					fmt.Println("Invalid amount. Must be greater than 0")
+					//return
+					continue
+				}
 
-			if depositAmount <= 0{
-				fmt.Println("Invalid amount. Must be greater than 0")
-				//return
-				continue
-			}
+				accountBalance += depositAmount  //accountBalance += accountBalance + depositAmount
+				fmt.Println("Balance updated! New amount:", accountBalance)
+				writeBalanceToFile(accountBalance)
 
-			accountBalance += depositAmount  //accountBalance += accountBalance + depositAmount
-			fmt.Println("Balance updated! New amount:", accountBalance)
+			case 3:
+				fmt.Print("Withdrawal amount: ")
+				var withdrawAmount float64
+				fmt.Scan(&withdrawAmount)
 
-		}else if choice == 3{
-			fmt.Print("Withdrawal amount: ")
-			var withdrawAmount float64
-			fmt.Scan(&withdrawAmount)
+				if withdrawAmount <= 0 {
+					fmt.Println("Invalid amount. Must be greater than 0")
+					continue
+				}
 
-			if withdrawAmount <= 0{
-				fmt.Println("Invalid amount. Must be greater than 0")
+				if withdrawAmount > accountBalance{
+					fmt.Println("Invalid amount. You can't withdraw more than you have")
+					continue
+				}
+				accountBalance -= withdrawAmount
+				fmt.Println("Money Withdraw! New amount:", accountBalance)
+				writeBalanceToFile(accountBalance)
+				
+			default:
+				fmt.Println("Goodbye!")
+				fmt.Println("Thanks for banking with us")
 				return
-			}
-
-			if withdrawAmount > accountBalance{
-				fmt.Println("Invalid amount. You can't withdraw more than you have")
-				return
-			}
-			accountBalance -= withdrawAmount
-			fmt.Println("Money Withdraw! New amount:", accountBalance)
-		}else{
-			fmt.Println("Goodbye!")
-			break
-			//return 
+				//break
 		}	
 	}
-
-	fmt.Println("Thanks for banking with us")
-
 }
