88e309fa81575d3e6add2dbca630e74b8fdb5120
Tino Nova Tech <124672998+Britinogn@users.noreply.github.com>
2026-01-02 14:40:38 +0100

    Add account balance management and user interaction for deposits and withdrawals

--- go-bank/bank.go
+++ go-bank/bank.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 func main() {
+	var accountBalance = 1000.0
+	
+
 	fmt.Println("Welcome to Bank")
 	fmt.Println("what do you want to do?")
 	fmt.Println("1. Check balance")
@@ -10,10 +13,48 @@ func main() {
 	fmt.Println("3. Withdraw money")
 	fmt.Println("4. Exit")
 
-
 	var choice int
-	fmt.Print("Your choice")
+	fmt.Print("Your choice " )
 	fmt.Scan(&choice)
+
+	//wantsCheckBalance := choice == 1
+
+	//if statement
+	if choice == 1{
+		fmt.Println("Your balance is:", accountBalance)
+	} else if choice == 2 {
+		fmt.Print("Your deposit: ")
+		var depositAmount float64
+		fmt.Scan(&depositAmount)
+
+		if depositAmount <= 0{
+			fmt.Println("Invalid amount. Must be greater than 0")
+			return
+		}
+
+		accountBalance += depositAmount  //accountBalance += accountBalance + depositAmount
+		fmt.Println("Balance updated! New amount:", accountBalance)
+	}else if choice == 3{
+		fmt.Print("Withdrawal amount: ")
+		var withdrawAmount float64
+		fmt.Scan(&withdrawAmount)
+
+		if withdrawAmount <= 0{
+			fmt.Println("Invalid amount. Must be greater than 0")
+			return
+		}
+
+		if withdrawAmount > accountBalance{
+			fmt.Println("Invalid amount. You can't withdraw more than you have")
+			return
+		}
+
+		accountBalance -= withdrawAmount
+		fmt.Println("Money Withdraw! New amount:", accountBalance)
+	}else{
+		fmt.Println("Goodbye!")
+		return 
+	}
 
 	fmt.Println("Your Choice:", choice)
 }
