4f62c16ca5e05896e077e4d4ee03f720dac6347d
Tino Nova Tech <124672998+Britinogn@users.noreply.github.com>
2026-01-03 17:51:59 +0100

    Add user struct implementation with methods for output and clearing names

---
+++ structs/structs.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+
+type user struct{
+	firstName string
+	lastName string
+	birthDate string
+	age int
+	createdAt time.Time
+}
+
+// (u user) receiver argument
+func (u user) outputUserDetails(){
+	
+
+	fmt.Println(u.firstName, u.lastName, u.birthDate)
+	//fmt.Println((*u).firstName, (*u).lastName, (*u).birthDate)
+}
+
+func (u *user) clearUserName(){
+	u.firstName = ""
+	u.lastName = ""
+}
+
+
+func  newUser(firstName, lastName, birthDate string ) *user {
+	return &user{
+		firstName: firstName,
+		lastName: lastName,
+		birthDate: birthDate,
+		createdAt: time.Now(),
+	}
+}
+
+func main() {
+	userFirstName := getUserData("Please enter your first name: ")
+	userLastName := getUserData("Please enter your last name: ")
+	userBirthDate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+
+
+	var appUser *user 
+
+	appUser = newUser(userFirstName, userLastName, userBirthDate)
+	//...
+
+	//fmt.Println(firstName, lastName ,birthDate)
+	//outputUserDetails(&appUser)
+	appUser.outputUserDetails()
+	appUser.clearUserName()
+	appUser.outputUserDetails()
+}
+
+func getUserData(promptText string) string {
+	fmt.Print(promptText)
+	var value string
+	fmt.Scan(&value)
+	return value
+}
